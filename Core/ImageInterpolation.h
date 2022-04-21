24e4390dffea597a53656568ed33cac218f213c1
ShifengJin <15210583738@163.com>
2022-04-21 23:34:59 +0800

    use stb replace opencv

---
+++ Core/ImageInterpolation.h
@@ -0,0 +1,15 @@
+#ifndef _IMAGEINTERPOLATION_H_
+#define _IMAGEINTERPOLATION_H_
+
+// *   *   *   *   *   *
+//   +       +       +
+
+//  0.6
+//  *    *    *    *    *    *    *    *    *    *   9x+10    9x+4   x=4     40
+//    +        +        +        +        +        +                            5y+6     5y     y=8
+
+// 13/5
+
+void ImageInterpolation_Nearest();
+
+#endif
--- example/TwoImageAddTest.cpp
+++ example/TwoImageAddTest.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
 #include "TwoImageAdd.h"
-#include <opencv2/opencv.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 int main(int argc, char* argv[]){
 
@@ -13,18 +16,42 @@ int main(int argc, char* argv[]){
     std::string image2Path = argv[2];
     std::string outImagePath = argv[3];
     float weight = atof(argv[4]);
-    cv::Mat image1 = cv::imread(image1Path);
-    cv::Mat image2 = cv::imread(image2Path);
 
-    if(image1.rows != image2.rows || image1.cols != image2.cols){
-        std::cout << "image1 and image2 size must same." << std::endl;
+    int image1Width, image1Height, image1Channels;
+    stbi_uc* image1 = stbi_load(image1Path.c_str(), &image1Width, &image1Height, &image1Channels, STBI_rgb);
+    if(image1 == NULL){
+        std::cout << "load " << image1Path << " error." << std::endl;
         return 0;
     }
 
-    cv::Mat outImage(image1.rows, image1.cols, CV_8UC3);
-    TwoImageAdd(image1.data, image2.data, outImage.data, image1.cols, image1.rows, weight);
+    int image2Width, image2Height, image2Channels;
+    stbi_uc* image2 = stbi_load(image2Path.c_str(), &image2Width, &image2Height, &image2Channels, STBI_rgb);
+    if(image2 == NULL){
+        std::cout << "load " << image2Path << " error." << std::endl;
+        stbi_image_free(image1);
+        return 0;
+    }
 
-    cv::imwrite(outImagePath, outImage);
+    if(image1Width != image2Width || image1Height != image2Height){
+        std::cout << "image1 and image2 size must same." << std::endl;
+        stbi_image_free(image1);
+        stbi_image_free(image2);
+        return 0;
+    }
+    
+    unsigned char* oImageBuffer = (unsigned char*)calloc(image1Width * image1Height * image1Channels, sizeof(unsigned char));
+
+    if(oImageBuffer){
+        TwoImageAdd(image1, image2, oImageBuffer, image1Width, image1Height, weight);
+        stbi_write_jpg(outImagePath.c_str(), image1Width, image1Height, STBI_rgb, oImageBuffer, 100);
+        free(oImageBuffer);
+        stbi_image_free(image1);
+        stbi_image_free(image2);
+    }else{
+        std::cout << "calloc " << image1Width * image1Height * image1Channels * sizeof(unsigned char) << " size buffer error." << std::endl;
+        stbi_image_free(image1);
+        stbi_image_free(image2);
+    }
 
     return 0;
 }
